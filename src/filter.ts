9f930157727e9e02fed01cbc3f3a6d492f6332a9
al <usernamehw@gmail.com>
2020-05-02 10:36:48 +0300

    💄

--- src/filter.ts
+++ src/filter.ts
@@ -1,10 +1,4 @@
 import { Task } from './parse';
-/**
- * Filter must support negation
- * Filter must support AND
- * Filter must support OR
- * Filter must support > <
- */
 
 export function filterItems(tasks: Task[], filter: string): Task[] {
 	if (filter.length === 0) {
