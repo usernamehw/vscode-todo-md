20badc4451d15ef9b8158023b6c39b920a99412d
al <usernamehw@gmail.com>
2020-05-05 15:26:49 +0300

    More tests for filter

--- src/test/suite/filterItems.test.ts
+++ src/test/suite/filterItems.test.ts
@@ -12,31 +12,74 @@ function newTask(task: Init) {
 	return new Task(task);
 }
 
-const justText = newTask({
+const justTextTask = newTask({
 	title: 'just text',
 });
-const threeTags = newTask({
+const threeTagsTask = newTask({
 	title: 'three tags',
 	tags: ['html', 'css', 'js'],
 });
-const oneTagHtml = newTask({
+const oneTagHtmlTask = newTask({
 	title: 'one tag',
 	tags: ['html'],
 });
-
+const oneContextTask = newTask({
+	title: 'one context',
+	contexts: ['home'],
+});
+const multipleContextTask = newTask({
+	title: 'multiple contexts',
+	contexts: ['home', 'work', 'email'],
+});
+const oneProjectTask = newTask({
+	title: 'one project',
+	projects: ['one'],
+});
+const multipleProjectTask = newTask({
+	title: 'multiple projects',
+	projects: ['one', 'two', 'three'],
+});
 // ──────────────────────────────────────────────────────────────────────
 describe('Filter tags', () => {
 	it('One tag', () => {
-		const items = [justText, threeTags, oneTagHtml];
+		const items = [justTextTask, threeTagsTask, oneTagHtmlTask];
 		const filtered = filterItems(items, '#html');
 		expect(filtered).to.have.length(2);
-		expect(filtered).to.have.same.members([threeTags, oneTagHtml]);
+		expect(filtered).to.have.same.members([threeTagsTask, oneTagHtmlTask]);
 	});
 	it('Multiple tags', () => {
-		const items = [justText, threeTags];
+		const items = [justTextTask, threeTagsTask];
 		const filtered = filterItems(items, '#html #js');
 		expect(filtered).to.have.length(1);
-		expect(filtered).to.have.same.members([threeTags]);
+		expect(filtered).to.have.same.members([threeTagsTask]);
+	});
+});
+describe('Filter contexts', () => {
+	it('One context', () => {
+		const items = [justTextTask, threeTagsTask, oneTagHtmlTask, multipleContextTask, oneContextTask];
+		const filtered = filterItems(items, '@work');
+		expect(filtered).to.have.length(1);
+		expect(filtered).to.have.same.members([multipleContextTask]);
+	});
+	it('Multiple contexts', () => {
+		const items = [justTextTask, threeTagsTask, oneTagHtmlTask, multipleContextTask, oneContextTask];
+		const filtered = filterItems(items, '@home @work');
+		expect(filtered).to.have.length(1);
+		expect(filtered).to.have.same.members([multipleContextTask]);
+	});
+});
+describe('Filter projects', () => {
+	it('One project', () => {
+		const items = [justTextTask, threeTagsTask, oneTagHtmlTask, multipleContextTask, oneContextTask, oneProjectTask, multipleProjectTask];
+		const filtered = filterItems(items, '+one');
+		expect(filtered).to.have.length(2);
+		expect(filtered).to.have.same.members([multipleProjectTask, oneProjectTask]);
+	});
+	it('Multiple projects', () => {
+		const items = [justTextTask, threeTagsTask, oneTagHtmlTask, multipleContextTask, oneContextTask, oneProjectTask, multipleProjectTask];
+		const filtered = filterItems(items, '+one +two');
+		expect(filtered).to.have.length(1);
+		expect(filtered).to.have.same.members([multipleProjectTask]);
 	});
 });
 
