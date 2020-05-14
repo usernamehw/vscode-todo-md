2d0d4a5dbf851b58cf5c9f0733cbaccb9f968082
al <usernamehw@gmail.com>
2020-05-14 08:33:38 +0300

    ongoing calendar

--- src/webview/agenda.ts
+++ src/webview/agenda.ts
@@ -60,11 +60,3 @@ function getWebviewContent(webview: vscode.Webview, cssFilePath: vscode.Uri, tas
 </html>`;
 }
 
-function getNonce() {
-	let text = '';
-	const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-	for (let i = 0; i < 32; i++) {
-		text += possible.charAt(Math.floor(Math.random() * possible.length));
-	}
-	return text;
-}
--- src/webview/calendar.ts
+++ src/webview/calendar.ts
@@ -4,13 +4,13 @@ import dayjs from 'dayjs';
 
 import { state } from '../extension';
 import { TheTask } from '../parse';
-import { DATE_FORMAT, parseDue } from '../timeUtils';
+import { DATE_FORMAT, parseDue, dayjsUtcToLocal } from '../timeUtils';
 
 export function createCalendarWebview() {
 	const panel = vscode.window.createWebviewPanel(
 		'calendar',
 		'Calendar',
-		vscode.ViewColumn.Beside, // Editor column to show the new webview panel in.
+		vscode.ViewColumn.Active, // Editor column to show the new webview panel in.
 		{
 			enableScripts: true,
 		}
@@ -18,23 +18,72 @@ export function createCalendarWebview() {
 	const cssFilePath = vscode.Uri.file(
 		path.join(state.extensionContext.extensionPath, 'media', 'calendar.css')
 	);
-	panel.webview.html = getWebviewContent(panel.webview, cssFilePath);
+	const jsFilePath = vscode.Uri.file(
+		path.join(state.extensionContext.extensionPath, 'media', 'calendar.js')
+	);
+	panel.webview.html = getWebviewContent(panel.webview, cssFilePath, jsFilePath);
 }
-function getWebviewContent(webview: vscode.Webview, cssFilePath: vscode.Uri) {
-	let htmlAsText = 'wow, such calendar';
+function getHeaderHtml() {
+	let str = '<tr>';
+	str += '<th>Mon</th>';
+	str += '<th>Tue</th>';
+	str += '<th>Wed</th>';
+	str += '<th>Thu</th>';
+	str += '<th>Fri</th>';
+	str += '<th>Sat</th>';
+	str += '<th>Sun</th>';
+	str += '</tr>';
+	return str;
+}
+function getCell(text: string, dateStr: string) {
+	return `<td data-id="${dateStr}">${text}</td>`;
+}
+function getWebviewContent(webview: vscode.Webview, cssUri: vscode.Uri, jsUri: vscode.Uri) {
+	let htmlAsText = `<table>${getHeaderHtml()}`;
+	const firstDateOfMonth = dayjs().startOf('month');
+	const firstDateOfMonthDay = firstDateOfMonth.day();
+	const lastDateOfMonth = dayjs().endOf('month');
+	const howManyEmptyRender = firstDateOfMonthDay === 0 ? 6 : firstDateOfMonthDay - 1;
+	htmlAsText += '<tr>';
+	htmlAsText += getCell('', '').repeat(howManyEmptyRender);
+	for (let i = 0, j = howManyEmptyRender + 1; ; j++, i++) {
+		const day = firstDateOfMonth.add(i, 'day');
+		const dayAsHtmlStr = getCell(day.date().toString(), day.format(DATE_FORMAT));
+		htmlAsText += dayAsHtmlStr;
+		if (j === 7) {
+			j = 0;
+			htmlAsText += '</tr><tr>';
+		}
+		if (day.isSame(lastDateOfMonth, 'day')) {
+			htmlAsText += '</tr>';
+			break;
+		}
+	}
 
-	// And the uri we use to load this script in the webview
-	const cssUri = webview.asWebviewUri(cssFilePath);
+	htmlAsText += '</table>';
+	const webviewUri = webview.asWebviewUri(cssUri);
+	const scriptUri = webview.asWebviewUri(jsUri);
+	const nonce = getNonce();
 	return `<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="${cssUri}">
+	<link rel="stylesheet" href="${webviewUri}">
     <title>Cat Coding</title>
 </head>
 <body>
-    ${htmlAsText}
+	${htmlAsText}
+	<script nonce="${nonce}" src="${scriptUri}"></script>
 </body>
 </html>`;
+}
+
+function getNonce() {
+	let text = '';
+	const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	for (let i = 0; i < 32; i++) {
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+	}
+	return text;
 }
