e92050e0e5c0cb33abd9b658f13d78f51822d309
al <usernamehw@gmail.com>
2020-07-24 10:19:20 +0300

    💄

--- src/vscodeUtils.ts
+++ src/vscodeUtils.ts
@@ -1,6 +1,8 @@
-import * as vscode from 'vscode';
-import { Uri } from 'vscode';
+import vscode, { Uri } from 'vscode';
 
+/**
+ * Create new untitled file with provided content and language;
+ */
 export async function openInUntitled(content: string, language?: string): Promise<void> {
 	const document = await vscode.workspace.openTextDocument({
 		language,
@@ -8,27 +10,29 @@ export async function openInUntitled(content: string, language?: string): Promis
 	});
 	vscode.window.showTextDocument(document);
 }
-
+/**
+ * Open file by absolute path in the editor.
+ */
 export async function openFileInEditor(path: string) {
 	const document = await vscode.workspace.openTextDocument(path);
 	vscode.window.showTextDocument(document);
 }
-
-export function insertSnippet(snippet: string): void {
-	vscode.commands.executeCommand('editor.action.insertSnippet', {
-		snippet,
-	});
-}
-
+/**
+ * Given only start and end lines - get the full Range with characters.
+ */
 export function getFullRangeFromLines(document: vscode.TextDocument, lineStart: number, lineEnd: number): vscode.Range {
 	const lineAtTheEnd = document.lineAt(lineEnd);
 	return new vscode.Range(lineStart, 0, lineEnd, lineAtTheEnd.range.end.character);
 }
-
+/**
+ * Set vscode context.
+ */
 export function setContext(context: string, value: any) {
 	vscode.commands.executeCommand('setContext', context, value);
 }
-
+/**
+ * Open URL in default browser.
+ */
 export function followLink(link: string) {
 	vscode.env.openExternal(Uri.parse(link));
 }
