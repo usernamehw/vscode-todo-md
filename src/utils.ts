9525417c83fca8fd720313b449ad4e98e99969a1
al <usernamehw@gmail.com>
2020-07-24 10:25:58 +0300

    💄

--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,16 @@
 import { promises as fs } from 'fs';
 
 export async function appendTaskToFile(text: string, filePath: string) {
-	await fs.appendFile(filePath, `${text}\n`);
+	await fs.appendFile(filePath, `${text}\n`);// TODO: check if file ends with \n before writing
 }
 
 export function getRandomInt(min: number, max: number): number {
 	return min + Math.floor(Math.random() * (max - min + 1));
 }
-
+/**
+ * Replace numbers with emoji.
+ * Works for multiple digits too.
+ */
 export function fancyNumber(n: number): string {
 	const digitMap = ['0️⃣', '1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣'];
 	return String(n)
