ec316e9c5c0508e216bfbf54f3d15e1867d47343
al <usernamehw@gmail.com>
2020-07-24 10:05:12 +0300

    💄

--- src/utils.ts
+++ src/utils.ts
@@ -10,5 +10,8 @@ export function getRandomInt(min: number, max: number): number {
 
 export function fancyNumber(n: number): string {
 	const digitMap = ['0️⃣', '1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣'];
-	return String(n).split('').map(digit => digitMap[digit]).join('');
+	return String(n)
+		.split('')
+		.map(digit => digitMap[digit])
+		.join('');
 }
