e155ed0621efafb65f932cba9e5b67e4286d8ece
al <usernamehw@gmail.com>
2020-10-14 13:17:24 +0300

    Completion: use entities from state

--- src/completionProviders.ts
+++ src/completionProviders.ts
@@ -16,7 +16,7 @@ export function updateCompletions(): void {
 		{
 			provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
 				const tagCompletions = [];
-				for (const tag of getAllTags()) {
+				for (const tag of state.tags.concat(extensionConfig.tags)) {
 					const tagCompletion = new vscode.CompletionItem(tag, vscode.CompletionItemKind.Field);
 					tagCompletion.commitCharacters = ['#'];
 					tagCompletions.push(tagCompletion);
@@ -32,7 +32,7 @@ export function updateCompletions(): void {
 		{
 			provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
 				const tagCompletions = [];
-				for (const tag of getAllProjects()) {
+				for (const tag of state.projects.concat(extensionConfig.projects)) {
 					const tagCompletion = new vscode.CompletionItem(tag, vscode.CompletionItemKind.Field);
 					tagCompletion.commitCharacters = ['+'];
 					tagCompletions.push(tagCompletion);
@@ -48,7 +48,7 @@ export function updateCompletions(): void {
 		{
 			provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
 				const contextCompletions = [];
-				for (const context of getAllContexts()) {
+				for (const context of state.contexts.concat(extensionConfig.contexts)) {
 					const contextCompletion = new vscode.CompletionItem(context, vscode.CompletionItemKind.Field);
 					contextCompletion.commitCharacters = ['@'];
 					contextCompletions.push(contextCompletion);
@@ -80,43 +80,3 @@ export function updateCompletions(): void {
 	);
 }
 
-function getAllTags(): Set<string> {
-	const set: Set<string> = new Set();
-	for (const line of state.tasks) {
-		for (const tag of line.tags) {
-			set.add(tag);
-		}
-	}
-	for (const tag of extensionConfig.tags) {
-		set.add(tag);
-	}
-	return set;
-}
-function getAllProjects(): Set<string> {
-	const set: Set<string> = new Set();
-	for (const line of state.tasks) {
-		if (line.projects) {
-			for (const project of line.projects) {
-				set.add(project);
-			}
-		}
-	}
-	for (const project of extensionConfig.projects) {
-		set.add(project);
-	}
-	return set;
-}
-function getAllContexts(): Set<string> {
-	const set: Set<string> = new Set();
-	for (const line of state.tasks) {
-		if (line.contexts) {
-			for (const context of line.contexts) {
-				set.add(context);
-			}
-		}
-	}
-	for (const context of extensionConfig.contexts) {
-		set.add(context);
-	}
-	return set;
-}
