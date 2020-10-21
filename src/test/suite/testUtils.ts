c65bc2b1d81fd055f08ae7e6c4de331d5413d933
al <usernamehw@gmail.com>
2020-10-21 11:32:16 +0300

    💄

--- src/test/suite/testUtils.ts
+++ src/test/suite/testUtils.ts
@@ -1,5 +1,5 @@
 export function headerDelimiter(header: string): string {
-	const TOTAL_WIDTH = 60;
+	const TOTAL_WIDTH = 55;
 	const leftCount = Math.floor((TOTAL_WIDTH - header.length) / 2);
 	const rightCount = TOTAL_WIDTH - leftCount - header.length;
 	return `\n${'─'.repeat(leftCount)} ${header.toUpperCase()} ${'─'.repeat(rightCount)}\n  `;
