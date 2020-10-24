11633e996a227dcde7400b8dfd95bbce40a143ca
al <usernamehw@gmail.com>
2020-10-24 11:41:33 +0300

    🔨

--- src/time/timeUtils.ts
+++ src/time/timeUtils.ts
@@ -14,14 +14,19 @@ export const monthRegexp = /^(Jan|January|Feb|February|Mar|March|Apr|April|May|J
 
 /**
  * Get date or datetime ISO 8601
+ *
  * Example: `2020-04-21` or `2020-04-30T09:11:17`
- * Uses local time
+ *
+ * Uses local time.
  */
 export function getDateInISOFormat(date: Date | Dayjs = new Date(), includeTime = false): string {
 	const format = includeTime ? DATE_TIME_FORMAT : DATE_FORMAT;
 	return dayjs(date).format(format);
 }
-
+/**
+ * Transform day of the week `sun` | `sunday` into its JS index
+ * - sunday => 0
+ */
 export function dayOfWeekToIndexOfWeek(dayOfWeek: string): number {
 	if (/^(sun|sunday)$/i.test(dayOfWeek)) {
 		return 0;
@@ -40,6 +45,10 @@ export function dayOfWeekToIndexOfWeek(dayOfWeek: string): number {
 	}
 	throw Error(`Unknown day of the week. [${dayOfWeek}]`);
 }
+/**
+ * Transform month `jan` | `january` into its JS index
+ * - january => 0
+ */
 export function monthStringToMonthIndex(month: string): number {
 	if (/^(Jan|January)$/i.test(month)) {
 		return 0;
@@ -78,7 +87,15 @@ export function dayOfTheWeek(date: dayjs.Dayjs): string {
  * Human readable date diff? `in 2 days`
  */
 export function dateDiff(date: dayjs.Dayjs): string {
-	return dayjs().to(date);
+	let diff;
+	if (dayjs().isSame(date, 'date')) {
+		diff = 'today';
+	} else if (dayjs().add(1, 'day').isSame(date, 'date')) {
+		diff = 'tomorrow';
+	} else {
+		diff = dayjs().to(date);
+	}
+	return diff;
 }
 /**
  * Return date, day of the week and a diff
@@ -86,13 +103,5 @@ export function dateDiff(date: dayjs.Dayjs): string {
  * e.g. `2020-10-23 Fri [in 2 days]`
  */
 export function dateAndDateDiff(date: dayjs.Dayjs): string {
-	let diff;
-	if (dayjs().isSame(date, 'date')) {
-		diff = 'today';
-	} else if (dayjs().add(1, 'day').isSame(date, 'date')) {
-		diff = 'tomorrow';
-	} else {
-		diff = dateDiff(date);
-	}
-	return `${date.format(DATE_FORMAT)} ${dayOfTheWeek(date)} [${diff}]`;
+	return `${date.format(DATE_FORMAT)} ${dayOfTheWeek(date)} [${dateDiff(date)}]`;
 }
