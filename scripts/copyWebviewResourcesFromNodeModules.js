0b17209468a19b0f490676d25a365a2fe054024f
al <usernamehw@gmail.com>
2020-11-10 15:20:07 +0300

    Compress vendor css in postinstall script

--- scripts/copyWebviewResourcesFromNodeModules.js
+++ scripts/copyWebviewResourcesFromNodeModules.js
@@ -1,9 +1,14 @@
 const fs = require('fs');
+const { exec } = require('child_process');
 
-fs.copyFile('node_modules/vscode-codicons/dist/codicon.css', 'media/vendor/codicon.css', (err) => {
-	if (err) throw err;
-	console.log('✅ codeicon.css copy success');// TODO: compress it
-});
+fs.unlink('./media/vendor/codicon.css', (err) => {
+	if (err) {
+		console.error(err);
+	}
+	exec('npm run copyAndCompressVendorCss');
+	console.log('✅ codeicon.css copy success');
+})
+
 fs.copyFile('node_modules/vscode-codicons/dist/codicon.ttf', 'media/vendor/codicon.ttf', (err) => {
 	if (err) throw err;
 	console.log('✅ codicon.ttf copy success');
