b7ed4890ecf783770efcdbbe94ab7c592d699018
al <usernamehw@gmail.com>
2021-02-16 15:08:37 +0300

    [webview] Click to select should work better now

--- media/app/Task.vue
+++ media/app/Task.vue
@@ -4,43 +4,44 @@
          class="list-item"
          :class="classes"
          @click.alt="revealTask"
-         @click.self.exact="selectThisTask"
+         @click.exact="selectThisTask"
          @contextmenu.prevent.stop="openTaskContextMenu($event, model)">
         <span v-if="model.subtasks.length"
               class="twistie codicon"
               :class="[model.collapseRange ? 'codicon-chevron-right' : 'codicon-chevron-down']"
-              @click="toggleTaskCollapse"></span><!--
+              @click.stop="toggleTaskCollapse"></span><!--
     --><input type="checkbox"
                :checked="model.done"
                aria-label="Task completion status"
                class="checkbox"
                :class="[config.customCheckboxEnabled ? 'custom-checkbox ' : 'native-checkbox']"
+               @click.stop
                @change="toggleDone"><!--
         --><span class="title"
                   v-html="taskTitle"></span><!--
         --><template v-for="tag of model.tags">
             <span class="tag"
                   :style="styleForTag(tag)"
-                  @click.exact="updateFilterValue('#' + tag)"
-				  @click.ctrl="updateFilterValue('#' + tag, true)">{{ tag }}</span>
+                  @click.exact.stop="updateFilterValue('#' + tag)"
+				  @click.ctrl.stop="updateFilterValue('#' + tag, true)">{{ tag }}</span>
         </template><!--
         --><template v-for="project of model.projects">
             <span class="project"
-                  @click.exact="updateFilterValue('+' + project)"
-                  @click.ctrl="updateFilterValue('+' + project, true)">{{ project }}</span>
+                  @click.exact.stop="updateFilterValue('+' + project)"
+                  @click.ctrl.stop="updateFilterValue('+' + project, true)">{{ project }}</span>
         </template><!--
         --><template v-for="context of model.contexts">
             <span class="context"
-                  @click.exact="updateFilterValue('@' + context)"
-                  @click.ctrl="updateFilterValue('@' + context, true)">{{ context }}</span>
+                  @click.exact.stop="updateFilterValue('@' + context)"
+                  @click.ctrl.stop="updateFilterValue('@' + context, true)">{{ context }}</span>
         </template><!--
         --><span v-if="model.count"
               class="count-container">
             <button class="decrement-count"
-                    @click="decrementCount">-</button>
+                    @click.stop="decrementCount">-</button>
             <span class="count">{{ model.count.current }} / {{ model.count.needed }}</span>
             <button class="increment-count"
-                    @click="incrementCount">+</button>
+                    @click.stop="incrementCount">+</button>
         </span>
         <template v-if="dueDate">
             <span v-html="dueDate"></span>
