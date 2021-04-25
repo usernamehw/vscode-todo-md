37efb3935fd1cb6f5afd6cfa81b1aacc866ad9e7
al <usernamehw@gmail.com>
2021-04-25 18:39:14 +0300

    Textare min height - 1 row

--- media/app/components/TaskDetails.vue
+++ media/app/components/TaskDetails.vue
@@ -1,6 +1,7 @@
 <template>
 <div class="task-details">
     <textarea ref="detailsTaskTitle"
+              rows="1"
               class="task-title"
               @input="onTaskTitleChange">{{ inputValue }}</textarea>
 </div>
