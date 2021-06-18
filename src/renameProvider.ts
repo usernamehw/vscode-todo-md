b5fcb90b70c54758fc57d7dc731fedb9aebcaa9c
al <usernamehw@gmail.com>
2021-06-18 12:36:05 +0300

    Show error when trying to rename anything except tag/project/context

--- src/renameProvider.ts
+++ src/renameProvider.ts
@@ -46,6 +46,18 @@ export function updateRenameProvider() {
 				}
 				return undefined;
 			},
+			async prepareRename(document, position) {
+				const range = getWordRangeAtPosition(document, position);
+				if (!range) {
+					return undefined;
+				}
+				const word = document.getText(range);
+				const parsedWord = parseWord(word, position.line, range.start.character);
+				if (parsedWord.type !== 'tags' && parsedWord.type !== 'project' && parsedWord.type !== 'context') {
+					return Promise.reject('You cannot rename this element');
+				}
+				return undefined;
+			},
 		},
 	);
 }
