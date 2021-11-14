5d1df688af6f12e64b3c30ae1acbedbf3943544f
al <usernamehw@gmail.com>
2021-11-14 21:04:16 +0300

    keep order of projects/contexts/tags in hover (#36)

--- src/hover/getTaskHover.ts
+++ src/hover/getTaskHover.ts
@@ -47,20 +47,34 @@ export function getTaskHover(task: TheTask) {
 		}
 	}
 
-	markdown.appendMarkdown(`${task.title}${count}${due}\n\n`);
+	const words = task.title.split(' ');
+	let taskTitle = task.title;
+
+	const resultWords = [];
+	for (const word of words) {
+		if (
+			word.length > 1 &&
+			(word[0] === '#' || word[0] === '+' || word[0] === '@')
+		) {
+			if (word[0] === '#') {
+				resultWords.push(`<span style="color:#fff;background-color:#029cdf;">&nbsp;${word}&nbsp;</span>`);
+			} else if (word[0] === '+') {
+				resultWords.push(`<span style="color:#fff;background-color:#36cc9a;">&nbsp;${word}&nbsp;</span>`);
+			} else {
+				resultWords.push(`<span style="color:#fff;background-color:#7284eb;">&nbsp;${word}&nbsp;</span>`);
+			}
+		} else {
+			resultWords.push(word);
+		}
+	}
+
+	taskTitle = resultWords.join(' ');
+
+	markdown.appendMarkdown(`${taskTitle}${count}${due}\n\n`);
 
 	if (task.start) {
 		markdown.appendMarkdown(`<span>$(watch) ${durationTo(task, false, extensionConfig.durationIncludeSeconds)}</span>\n\n`);
 	}
 
-	for (const tag of task.tags) {
-		markdown.appendMarkdown(`<span style="color:#fff;background-color:#029cdf;">&nbsp;#${tag}&nbsp;</span>&nbsp;`);
-	}
-	for (const project of task.projects) {
-		markdown.appendMarkdown(`<span style="color:#fff;background-color:#36cc9a;">&nbsp;+${project}&nbsp;</span>&nbsp;`);
-	}
-	for (const context of task.contexts) {
-		markdown.appendMarkdown(`<span style="color:#fff;background-color:#7284eb;">&nbsp;@${context}&nbsp;</span>&nbsp;`);
-	}
 	return markdown;
 }
