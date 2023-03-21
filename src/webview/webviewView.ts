0ddb2313eba8f77079d4fc9f72ba3c42181282f6
al <usernamehw@gmail.com>
2023-03-21 09:32:16 +0300

    feat: allow video from https source to render in webview

--- src/webview/webviewView.ts
+++ src/webview/webviewView.ts
@@ -228,7 +228,7 @@ export class TasksWebviewViewProvider implements WebviewViewProvider {
 			<head>
 				<meta charset="UTF-8">
 				<meta name="viewport" content="width=device-width, initial-scale=1.0">
-				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src https://*; font-src ${webview.cspSource}; style-src 'unsafe-inline' ${webview.cspSource}; script-src 'nonce-${nonce}';">
+				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src https://*; media-src https://*; font-src ${webview.cspSource}; style-src 'unsafe-inline' ${webview.cspSource}; script-src 'nonce-${nonce}';">
 				<link href="${codiconCSSUri}" rel="stylesheet" />
 				<link href="${CSSUri}" rel="stylesheet">
 				${userCSSLink}
