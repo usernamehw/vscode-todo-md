57c1b61dd5c1f6126bd7c80885f0ad7bf790fe3e
Alexander <usernamehw@gmail.com>
2023-10-10 20:47:00 +0300

    feat: tweak style of task details

--- media/app/components/TaskDetails/TaskDetails.vue
+++ media/app/components/TaskDetails/TaskDetails.vue
@@ -1,5 +1,6 @@
 <template>
 <div class="details">
+    <header class="details__header">Edit task as raw text:</header>
     <textarea ref="detailsTaskTitle"
               rows="1"
               class="details__input"
