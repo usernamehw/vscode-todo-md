500bb73e7fdd9d7ca4ffa4a17f65ca93c9f7d104
Alexander <usernamehw@gmail.com>
2024-02-08 20:11:54 +0300

    fix: hide shadow indicating scroll on input in webview

--- media/app/App.ts
+++ media/app/App.ts
@@ -184,6 +184,7 @@ export default defineComponent({
 			this.$nextTick(() => {
 				this.mainStore.selectFirstTask();
 				this.highlightMatchesThrottled();
+				this.onTaskListScroll();// hide shadow indicating scroll on header
 			});
 		},
 		/**
